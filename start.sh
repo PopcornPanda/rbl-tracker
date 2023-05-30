3bead51ea14ac6f59d281f3fa92f62e6c1bee70b
Łukasz Szczepański <lukasz.szczepanski@speednet.pl>
2023-05-30 15:06:47 +0200

    Start script

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+MS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
+TOKEN=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1|base64 -w 0`
+sed -i "s/MSPASS/${MS}/g;s/TOKEN/${TOKEN}/g" Docker/docker-compose.yml Docker/appsettings.mssql.json
+cd Docker && docker compose up -d
