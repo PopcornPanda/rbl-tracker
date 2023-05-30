97f21473e0ab5bf62ee67ee41616d88d2c4ab143
Łukasz Szczepański <lukasz.szczepanski@speednet.pl>
2023-05-30 15:16:45 +0200

    Update for docker compose

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 MS=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
 TOKEN=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1|base64 -w 0`
-sed -i "s/MSPASS/${MS}/g;s/TOKEN/${TOKEN}/g" Docker/docker-compose.yml Docker/appsettings.mssql.json
+sed -i "s/MSPASS/${MS}/g;s/TOKEN/${TOKEN}/g" Docker/docker-compose.yml
+sed "s/MSPASS/${MS}/g;s/TOKEN/${TOKEN}/g" appsettings.json > Docker/appsettings.mssql.json
 cd Docker && docker compose up -d
